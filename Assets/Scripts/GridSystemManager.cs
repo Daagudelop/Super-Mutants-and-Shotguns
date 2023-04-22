4534ac1fc036c5147e2b6b794d17f854905a61df
Daniel Felipe Agudelo Parra <d4niel.f.4.p4rr4@gmail.com>
2023-04-22 15:36:46 -0500

    Creación y renderizado via debbug de cuadriculas usando gameObjects, estado: funcionando

---
+++ Assets/Scripts/GridSystemManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GridSystemManager : MonoBehaviour
+{
+
+    public static GridSystemManager sharedInstanceGridSystemManager;
+
+    public Grid gridPrefab;
+    public int matrixColumns;
+    public int matrixLines;
+    public int cellSize;
+
+    //********************************
+    //  Unity methods
+
+    private void Awake()
+    {
+        GenerateGridMesh();
+    }
+
+    void Start()
+    {
+        initializeSingleton();
+    }
+
+    void Update()
+    {
+        
+    }
+    //********************************
+
+    private void initializeSingleton()
+    {
+        if (sharedInstanceGridSystemManager == null)
+        {
+            sharedInstanceGridSystemManager = this;
+        }
+    }
+
+    private void GenerateGridMesh()
+    {
+
+        int[,] abstractMatrix = new int[matrixColumns, matrixLines];
+
+        for (int i = 0; i < abstractMatrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < abstractMatrix.GetLength(1); j++)
+            {
+                InstantiateGrid(i, j);
+            }
+        }
+        
+    }
+
+    private void InstantiateGrid(int i, int j)
+    {
+        gridPrefab = Instantiate(gridPrefab);
+        gridPrefab.transform.position = CalculateWorldPosition(i, j) + new Vector3(cellSize,cellSize) * 0.5f;
+
+
+        // debbuging sector ******************************************
+        Debug.DrawLine( //linea inferior
+            CalculateWorldPosition(i, j), 
+            CalculateWorldPosition(i + cellSize, j),
+            Color.white, 10f);
+        Debug.DrawLine( //linea izquierda
+            CalculateWorldPosition(i, j), 
+            CalculateWorldPosition(i, j + cellSize), 
+            Color.white, 10f);
+        Debug.DrawLine( //linea derecha
+            CalculateWorldPosition(i + cellSize, j + cellSize), 
+            CalculateWorldPosition(i + cellSize, j), 
+            Color.white, 10f);
+        Debug.DrawLine( //linea superior
+            CalculateWorldPosition(i + cellSize, j + cellSize), 
+            CalculateWorldPosition(i, j + cellSize), 
+            Color.white, 10f);
+        //************************************************************
+    }
+
+    
+
+
+    private Vector3 CalculateWorldPosition(int i, int j)
+    {
+        return new Vector3(i, j, 0) * cellSize;
+    }
+
+    // debbuging sector ******************************************
+
+    //TODO: logica de debbug lines para crear las lineas de casillas.
+    //private void DebbugCentral()
+    //{
+    //    DebugLines();
+    //    DebugLines();
+    //    DebugLines();
+    //    DebugLines();
+    //}
+    //private void DebugLines(int i, int j, int x, int y)
+    //{
+    //    Debug.DrawLine( 
+    //        CalculateWorldPosition(i, j),
+    //        CalculateWorldPosition(x, y),
+    //        Color.white);
+    //}
+}
